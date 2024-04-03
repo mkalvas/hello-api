5280e1cb7dc406521c15f21fde8f4cf5d6059b38
Mike Kalvas <mkalvas@samcart.com>
2024-04-02 23:32:29 -0400

    feat: add restart to docker compose for reboots

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,15 @@
 while (( $# )); do
   case $1 in
     -d|--down) down=true;;
+    -r|--restart) restart=true;;
   esac
   shift
 done
 
 if [[ -n "$down" ]]; then
   docker --context mk-prod compose down
+elif [[ -n "$restart" ]]; then
+  docker --context mk-prod compose up -d
 else
   docker --context mk-prod compose up --build -d
 fi
